7c57fe83b680acbf514c0885066e9fa0ea2b3b3e
ivanikoladze <iwanikoladze@gmail.com>
2022-10-31 18:20:42 +0400

    toDo

--- toDo.js
+++ toDo.js
@@ -13,11 +13,27 @@ list.append(ulElem);
 // });
 
 
+class todo{
+    constructor(input, list){
+        this.input = input;
+        this.list = list; 
+    }
+    addToList(){
+        return this.list.innerHTML += `<li> ${this.input} </li>`;
+    }
+}
+
+
 add.addEventListener("click", () => {
-    let input = document.querySelector(".todo").value;
-    let newElem = `<li> ${input} </li>`
-    ulElem.innerHTML+=newElem;
-    document.querySelector(".todo").value = "";
+    let input = document.querySelector(".todo");
+    if(input.value == ""){
+        alert("არ არის შევსებული")
+    }else{
+        let todoTask = new todo(input.value, ulElem)
+        todoTask.addToList()
+    }
+    input.value = "";
+
 })
 
 
